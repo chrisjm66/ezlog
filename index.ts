0e42ce6a6573f7351bb340666a4702589132675e
Christopher Mangan <chrisjm66@gmail.com>
2025-02-21 14:38:52 -0500

    finally fixed

--- index.ts
+++ index.ts
@@ -2,7 +2,6 @@ import express, {Express, Request, Response } from "express"
 import dotenv from 'dotenv'
 import path, {dirname} from 'path'
 import { fileURLToPath } from "url"
-import vite from 'vite-express'
 
 // env
 dotenv.config()
@@ -12,8 +11,8 @@ const port = process.env.PORT || 8100
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-app.use(express.static(path.join(__dirname, '..', '..', 'public')))
-
+app.use(express.static(path.join(__dirname, 'dist', 'index.html')))
+console.log(path.join(__dirname, 'dist', 'index.html'))
 if (process.env.NODE_ENV == 'development') {
     app.get('/*', (req: Request, res: Response) => {
         res.send('hi')
@@ -22,12 +21,10 @@ if (process.env.NODE_ENV == 'development') {
     app.use(express.static(path.join(__dirname, 'dist')))
 
     app.get('/*', (req :Request, res: Response) => {
-        res.sendFile(path.join(__dirname, '..', '..', 'dist', 'index.html'));
+        res.sendFile(path.join(__dirname, 'dist', 'index.html'));
     })
 }
 
 const server = app.listen(port, () => {
     console.log("listening on port " + port)
 })
-
-vite.bind(app, server)
--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,4 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
-  server: {
-    cors: {
-      // the origin you will be accessing via browser
-      origin: 'http://localhost:8100',
-    },
-  },
-  build: {
-    // generate .vite/manifest.json in outDir
-    manifest: true,
-  },
 })
