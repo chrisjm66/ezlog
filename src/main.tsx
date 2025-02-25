2248dd269365b90a3117b24cbcaf31efdea4a557
Chris Mangan <120677529+chrisjm66@users.noreply.github.com>
2025-02-25 09:56:36 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,12 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import axios from 'axios'
 
-axios.defaults.baseURL = "http://localhost:8100"
 
+if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+  axios.defaults.baseURL = "http://localhost:8100"
+} else {
+  axios.defaults.baseURL = "https://localhost:8100"
+}
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App/>
