f3b23d37b1769001aca0ad6ebf370fdeded97c3e
Chris Mangan <120677529+chrisjm66@users.noreply.github.com>
2025-02-25 10:08:02 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import axios from 'axios'
 if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
   axios.defaults.baseURL = "http://localhost:8100"
 } else {
-  axios.defaults.baseURL = "https://localhost:8100"
+  axios.defaults.baseURL = "https://ezlog.chrismangan.net"
 }
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
